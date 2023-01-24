86fe5462fde15269e860b4112069934bbf0c62d8
Sergio Pérez <sergio-luis98@hotmail.com>
2023-01-24 07:44:54 -0500

    up

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CreateBrandDto } from './dto/create-brand.dto'
+import { UpdateBrandDto } from './dto/update-brand.dto'
+import { Brand } from './entities/brand.entity'
+import { v4 as uuid } from 'uuid'
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ]
+  create(createBrandDto: CreateBrandDto) {
+    return 'This action adds a new brand'
+  }
+
+  findAll() {
+    return this.brands
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id)
+    if (!brand) throw new NotFoundException(`Brand with id ${id} not found`)
+    return brand
+  }
+
+  update(id: number, updateBrandDto: UpdateBrandDto) {
+    return `This action updates a #${id} brand`
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} brand`
+  }
+}
